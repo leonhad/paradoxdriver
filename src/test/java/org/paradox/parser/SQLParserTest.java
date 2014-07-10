ce85464f5e662e1a1b9bee314a9424f1d9f44895
mederafael@gmail.com <mederafael@gmail.com@c9663276-e903-11de-8de6-035db03795fd>
2014-07-10 12:15:19 +0000

    Atualização das importações do pacote junit.framework para o org.junit.

--- src/test/java/org/paradox/parser/SQLParserTest.java
+++ src/test/java/org/paradox/parser/SQLParserTest.java
@@ -1,7 +1,7 @@
 package org.paradox.parser;
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 import org.paradox.parser.nodes.SQLNode;
 import org.paradox.parser.nodes.SelectNode;
