980e4eb20fad41eae524b74136c4cdc8cf7a2e0e
Leonardo Costa <leonhad@gmail.com>
2016-06-22 08:51:36 -0300

    Fixed some Sonar issues.

---
+++ src/test/java/com/googlecode/paradox/integration/IntegrationTest.java
@@ -0,0 +1,10 @@
+package com.googlecode.paradox.integration;
+
+/**
+ * Integration test interface. Used by JUnit Framework.
+ *
+ * @author Leonardo Alves da Costa
+ * @since 22/06/2016
+ */
+public interface IntegrationTest {
+}
