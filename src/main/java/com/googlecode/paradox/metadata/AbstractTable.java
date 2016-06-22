cf4a802e1793f5d9e8024c5f3185a94c250fa002
Leonardo Costa <leonhad@gmail.com>
2016-06-21 21:59:02 -0300

    Fixed some Sonar issues.

--- src/main/java/com/googlecode/paradox/metadata/AbstractTable.java
+++ src/main/java/com/googlecode/paradox/metadata/AbstractTable.java
@@ -14,37 +14,183 @@ import java.util.List;
  */
 public abstract class AbstractTable {
 
-	private final File file;
-	private String name;
+    protected int recordSize;
+    protected int headerSize;
+    protected byte type = -1;
+    protected byte blockSize;
+    protected int rowCount;
+    protected int usedBlocks;
+    protected int totalBlocks;
+    protected int firstBlock;
+    protected int lastBlock;
+    protected int fieldCount;
+    protected int primaryFieldCount;
+    protected byte writeProtected;
+    protected byte versionId;
+    protected int autoIncrementValue;
+    protected int firstFreeBlock;
+    protected byte referencialIntegrity;
 
-	public AbstractTable(final File file, final String name) {
-		this.file = file;
-		this.name = StringUtils.removeDb(name);
-	}
+    private final File file;
+    private String name;
 
-	public String getName() {
-		return name;
-	}
+    public AbstractTable(final File file, final String name) {
+        this.file = file;
+        this.name = StringUtils.removeDb(name);
+    }
+    
+    public abstract boolean isValid();
 
-	public void setName(final String name) {
-		this.name = name;
-	}
+    public abstract List<ParadoxField> getFields();
 
-	public File getFile() {
-		return file;
-	}
+    public ParadoxField findField(String name) {
+        if (getFields() != null) {
+            for (ParadoxField field : getFields()) {
+                if (field.getName().equalsIgnoreCase(name)) {
+                    return field;
+                }
+            }
+        }
+        return null;
+    }
 
-	public abstract boolean isValid();
+    public String getName() {
+        return name;
+    }
 
-	public abstract List<ParadoxField> getFields();
+    public void setName(final String name) {
+        this.name = name;
+    }
 
-	public ParadoxField findField(String name) {
-		if (getFields() != null) {
-			for (ParadoxField field: getFields()) {
-				if (field.getName().equalsIgnoreCase(name))
-					return field;
-			}
-		}
-		return null;
-	}
+    public File getFile() {
+        return file;
+    }
+
+    public int getRecordSize() {
+        return recordSize;
+    }
+
+    public void setRecordSize(int recordSize) {
+        this.recordSize = recordSize;
+    }
+
+    public int getHeaderSize() {
+        return headerSize;
+    }
+
+    public void setHeaderSize(int headerSize) {
+        this.headerSize = headerSize;
+    }
+
+    public byte getType() {
+        return type;
+    }
+
+    public void setType(byte type) {
+        this.type = type;
+    }
+
+    public byte getBlockSize() {
+        return blockSize;
+    }
+
+    public void setBlockSize(byte blockSize) {
+        this.blockSize = blockSize;
+    }
+
+    public int getRowCount() {
+        return rowCount;
+    }
+
+    public void setRowCount(int rowCount) {
+        this.rowCount = rowCount;
+    }
+
+    public int getUsedBlocks() {
+        return usedBlocks;
+    }
+
+    public void setUsedBlocks(int usedBlocks) {
+        this.usedBlocks = usedBlocks;
+    }
+
+    public int getTotalBlocks() {
+        return totalBlocks;
+    }
+
+    public void setTotalBlocks(int totalBlocks) {
+        this.totalBlocks = totalBlocks;
+    }
+
+    public int getFirstBlock() {
+        return firstBlock;
+    }
+
+    public void setFirstBlock(int firstBlock) {
+        this.firstBlock = firstBlock;
+    }
+
+    public int getLastBlock() {
+        return lastBlock;
+    }
+
+    public void setLastBlock(int lastBlock) {
+        this.lastBlock = lastBlock;
+    }
+
+    public int getFieldCount() {
+        return fieldCount;
+    }
+
+    public void setFieldCount(int fieldCount) {
+        this.fieldCount = fieldCount;
+    }
+
+    public int getPrimaryFieldCount() {
+        return primaryFieldCount;
+    }
+
+    public void setPrimaryFieldCount(int primaryFieldCount) {
+        this.primaryFieldCount = primaryFieldCount;
+    }
+
+    public byte getWriteProtected() {
+        return writeProtected;
+    }
+
+    public void setWriteProtected(byte writeProtected) {
+        this.writeProtected = writeProtected;
+    }
+
+    public byte getVersionId() {
+        return versionId;
+    }
+
+    public void setVersionId(byte versionId) {
+        this.versionId = versionId;
+    }
+
+    public int getAutoIncrementValue() {
+        return autoIncrementValue;
+    }
+
+    public void setAutoIncrementValue(int autoIncrementValue) {
+        this.autoIncrementValue = autoIncrementValue;
+    }
+
+    public int getFirstFreeBlock() {
+        return firstFreeBlock;
+    }
+
+    public void setFirstFreeBlock(int firstFreeBlock) {
+        this.firstFreeBlock = firstFreeBlock;
+    }
+
+    public byte getReferencialIntegrity() {
+        return referencialIntegrity;
+    }
+
+    public void setReferencialIntegrity(byte referencialIntegrity) {
+        this.referencialIntegrity = referencialIntegrity;
+    }
 }
