030d1d610006d2380a874f906ba50c3231c003d6
Leonardo Costa <leonhad@gmail.com>
2016-06-22 18:38:12 -0300

    Added an unit test for Utils.java.

--- src/test/java/com/googlecode/paradox/integration/MainTest.java
+++ src/test/java/com/googlecode/paradox/integration/MainTest.java
@@ -15,29 +15,24 @@ import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
 import com.googlecode.paradox.Driver;
-import com.googlecode.paradox.ParadoxConnection;
 
 /**
  * Generic tests for Paradox Driver
  *
  * @author Leonardo Alves da Costa
- * @since 14/3/2009
+ * @since 1.0
+ * @version 1.1
  */
 @Category(IntegrationTest.class)
 public class MainTest {
 
     public static final String CONNECTION_STRING = "jdbc:paradox:target/test-classes/";
-    private Connection conn;
-
     @BeforeClass
     public static void setUp() throws Exception {
         Class.forName(Driver.class.getName());
     }
 
-    @Before
-    public void connect() throws Exception {
-        conn = DriverManager.getConnection(MainTest.CONNECTION_STRING + "db");
-    }
+    private Connection conn;
 
     @After
     public void closeConnection() throws Exception {
@@ -46,10 +41,9 @@ public class MainTest {
         }
     }
 
-    @Test
-    public void testWrap() throws Exception {
-        Assert.assertTrue(conn.isWrapperFor(ParadoxConnection.class));
-        conn.unwrap(ParadoxConnection.class);
+    @Before
+    public void connect() throws Exception {
+        conn = DriverManager.getConnection(MainTest.CONNECTION_STRING + "db");
     }
 
     @Test
@@ -64,29 +58,6 @@ public class MainTest {
             } else {
                 Assert.fail("No catalog selected.");
             }
-        } finally {
-            if (rs != null) {
-                rs.close();
-            }
-        }
-    }
-
-    @Test
-    public void testPrimaryKey() throws Exception {
-        ResultSet rs = null;
-
-        try {
-            final DatabaseMetaData meta = conn.getMetaData();
-
-            rs = meta.getPrimaryKeys("db", "APP", "CUSTOMER.db");
-            Assert.assertTrue(rs.next());
-            Assert.assertEquals("db", rs.getString("TABLE_CAT"));
-            Assert.assertEquals("APP", rs.getString("TABLE_SCHEM"));
-            Assert.assertEquals("CUSTOMER", rs.getString("TABLE_NAME"));
-            Assert.assertEquals("CustNo", rs.getString("COLUMN_NAME"));
-            Assert.assertEquals("0", rs.getString("KEY_SEQ"));
-            Assert.assertEquals("CustNo", rs.getString("PK_NAME"));
-            // Assert.assertFalse(rs.next());
         } finally {
             if (rs != null) {
                 rs.close();
@@ -147,39 +118,81 @@ public class MainTest {
     }
 
     @Test
-    public void testViewColumns() throws Exception {
+    public void testPrimaryKey() throws Exception {
         ResultSet rs = null;
 
         try {
             final DatabaseMetaData meta = conn.getMetaData();
 
-            rs = meta.getColumns("db", "APP", "CONTASAPAGAR.QBE", "%");
-            while (rs.next()) {
-                /*
-                 * System.out.println("TABLE_CAT: " +
-                 * rs.getString("TABLE_CAT")); System.out.println(
-                 * "TABLE_SCHEM: " + rs.getString("TABLE_SCHEM"));
-                 * System.out.println("TABLE_NAME: " +
-                 * rs.getString("TABLE_NAME")); System.out.println(
-                 * "NON_UNIQUE: " + rs.getString("NON_UNIQUE"));
-                 * System.out.println("INDEX_QUALIFIER: " +
-                 * rs.getString("INDEX_QUALIFIER")); System.out.println(
-                 * "INDEX_NAME: " + rs.getString("INDEX_NAME"));
-                 * System.out.println("TYPE: " + rs.getString("TYPE"));
-                 * System.out.println("ORDINAL_POSITION: " +
-                 * rs.getString("ORDINAL_POSITION")); System.out.println(
-                 * "COLUMN_NAME: " + rs.getString("COLUMN_NAME"));
-                 * System.out.println("ASC_OR_DESC: " +
-                 * rs.getString("ASC_OR_DESC")); System.out.println(
-                 * "CARDINALITY: " + rs.getString("CARDINALITY"));
-                 * System.out.println("PAGES: " + rs.getString("PAGES"));
-                 * System.out.println("FILTER_CONDITION: " +
-                 * rs.getString("FILTER_CONDITION"));
-                 */
-            }
+            rs = meta.getPrimaryKeys("db", "APP", "CUSTOMER.db");
+            Assert.assertTrue(rs.next());
+            Assert.assertEquals("db", rs.getString("TABLE_CAT"));
+            Assert.assertEquals("APP", rs.getString("TABLE_SCHEM"));
+            Assert.assertEquals("CUSTOMER", rs.getString("TABLE_NAME"));
+            Assert.assertEquals("CustNo", rs.getString("COLUMN_NAME"));
+            Assert.assertEquals("0", rs.getString("KEY_SEQ"));
+            Assert.assertEquals("CustNo", rs.getString("PK_NAME"));
+            // Assert.assertFalse(rs.next());
         } finally {
             if (rs != null) {
                 rs.close();
+            }
+        }
+    }
+
+    @Test
+    public void testResultSet() throws Exception {
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            stmt = conn.createStatement();
+
+            rs = stmt.executeQuery("SELECT AC as 'ACode', State, CITIES FROM AREACODES");
+
+            Assert.assertTrue("No First row", rs.next());
+            Assert.assertEquals("Column 'AC':", rs.getString("ac"), "201");
+            Assert.assertEquals("Column 'State':", rs.getString("State"), "NJ");
+            Assert.assertEquals("Column 'Cities':", rs.getString("Cities"),
+                    "Hackensack, Jersey City (201/551 overlay)");
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (stmt != null) {
+                stmt.close();
+            }
+        }
+    }
+
+    @Test
+    public void testResultSet2() throws Exception {
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            stmt = conn.createStatement();
+
+            rs = stmt.executeQuery("SELECT \"id\", name, moneys FROM \"general.db\"");
+
+            Assert.assertTrue("First record:", rs.next());
+            Assert.assertEquals("1 row: ", "1 - Mari 100.0",
+                    rs.getLong(1) + " - " + rs.getString(2) + " " + rs.getFloat(3));
+            Assert.assertTrue("Second record:", rs.next());
+            Assert.assertEquals("2 row: ", "2 - Katty 150.0",
+                    rs.getLong(1) + " - " + rs.getString(2) + " " + rs.getFloat(3));
+            Assert.assertTrue("Third record:", rs.next());
+            Assert.assertEquals("2 row: ", "333333333 - Elizabet 75.0",
+                    rs.getLong(1) + " - " + rs.getString(2) + " " + rs.getFloat(3));
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (stmt != null) {
+                stmt.close();
+            }
+            if (conn != null) {
+                conn.close();
             }
         }
     }
@@ -253,58 +266,39 @@ public class MainTest {
     }
 
     @Test
-    public void testResultSet() throws Exception {
-        Statement stmt = null;
+    public void testViewColumns() throws Exception {
         ResultSet rs = null;
 
         try {
-            stmt = conn.createStatement();
+            final DatabaseMetaData meta = conn.getMetaData();
 
-            rs = stmt.executeQuery("SELECT AC as 'ACode', State, CITIES FROM AREACODES");
-
-            Assert.assertTrue("No First row", rs.next());
-            Assert.assertEquals("Column 'AC':", rs.getString("ac"), "201");
-            Assert.assertEquals("Column 'State':", rs.getString("State"), "NJ");
-            Assert.assertEquals("Column 'Cities':", rs.getString("Cities"),
-                    "Hackensack, Jersey City (201/551 overlay)");
+            rs = meta.getColumns("db", "APP", "CONTASAPAGAR.QBE", "%");
+            while (rs.next()) {
+                /*
+                 * System.out.println("TABLE_CAT: " +
+                 * rs.getString("TABLE_CAT")); System.out.println(
+                 * "TABLE_SCHEM: " + rs.getString("TABLE_SCHEM"));
+                 * System.out.println("TABLE_NAME: " +
+                 * rs.getString("TABLE_NAME")); System.out.println(
+                 * "NON_UNIQUE: " + rs.getString("NON_UNIQUE"));
+                 * System.out.println("INDEX_QUALIFIER: " +
+                 * rs.getString("INDEX_QUALIFIER")); System.out.println(
+                 * "INDEX_NAME: " + rs.getString("INDEX_NAME"));
+                 * System.out.println("TYPE: " + rs.getString("TYPE"));
+                 * System.out.println("ORDINAL_POSITION: " +
+                 * rs.getString("ORDINAL_POSITION")); System.out.println(
+                 * "COLUMN_NAME: " + rs.getString("COLUMN_NAME"));
+                 * System.out.println("ASC_OR_DESC: " +
+                 * rs.getString("ASC_OR_DESC")); System.out.println(
+                 * "CARDINALITY: " + rs.getString("CARDINALITY"));
+                 * System.out.println("PAGES: " + rs.getString("PAGES"));
+                 * System.out.println("FILTER_CONDITION: " +
+                 * rs.getString("FILTER_CONDITION"));
+                 */
+            }
         } finally {
             if (rs != null) {
                 rs.close();
-            }
-            if (stmt != null) {
-                stmt.close();
-            }
-        }
-    }
-
-    @Test
-    public void testResultSet2() throws Exception {
-        Statement stmt = null;
-        ResultSet rs = null;
-
-        try {
-            stmt = conn.createStatement();
-
-            rs = stmt.executeQuery("SELECT \"id\", name, moneys FROM \"general.db\"");
-
-            Assert.assertTrue("First record:", rs.next());
-            Assert.assertEquals("1 row: ", "1 - Mari 100.0",
-                    rs.getLong(1) + " - " + rs.getString(2) + " " + rs.getFloat(3));
-            Assert.assertTrue("Second record:", rs.next());
-            Assert.assertEquals("2 row: ", "2 - Katty 150.0",
-                    rs.getLong(1) + " - " + rs.getString(2) + " " + rs.getFloat(3));
-            Assert.assertTrue("Third record:", rs.next());
-            Assert.assertEquals("2 row: ", "333333333 - Elizabet 75.0",
-                    rs.getLong(1) + " - " + rs.getString(2) + " " + rs.getFloat(3));
-        } finally {
-            if (rs != null) {
-                rs.close();
-            }
-            if (stmt != null) {
-                stmt.close();
-            }
-            if (conn != null) {
-                conn.close();
             }
         }
     }
