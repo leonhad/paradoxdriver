519a9f29f69589a4e464adb27dbc192b0bd313a6
Leonardo Alves da Costa <leonhad@gmail.com>
2016-06-28 13:23:55 -0300

    Fixed some Sonar issues.

--- src/main/java/com/googlecode/paradox/data/IndexData.java
+++ src/main/java/com/googlecode/paradox/data/IndexData.java
@@ -21,7 +21,10 @@ package com.googlecode.paradox.data;
 
 import static java.nio.ByteBuffer.allocate;
 import static java.nio.charset.Charset.forName;
-
+import com.googlecode.paradox.ParadoxConnection;
+import com.googlecode.paradox.metadata.ParadoxField;
+import com.googlecode.paradox.metadata.ParadoxIndex;
+import com.googlecode.paradox.utils.filefilters.SecondaryIndexFilter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -32,11 +35,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.googlecode.paradox.ParadoxConnection;
-import com.googlecode.paradox.metadata.ParadoxField;
-import com.googlecode.paradox.metadata.ParadoxIndex;
-import com.googlecode.paradox.utils.filefilters.SecondaryIndexFilter;
-
 /**
  * Reads index data files.
  *
@@ -45,7 +43,7 @@ import com.googlecode.paradox.utils.filefilters.SecondaryIndexFilter;
  * @version 1.1
  */
 public final class IndexData {
-
+    
     /**
      * Utility class.
      */
@@ -64,7 +62,8 @@ public final class IndexData {
      * @throws SQLException
      *             in case of reading failures.
      */
-    public static List<ParadoxIndex> listIndexes(final ParadoxConnection conn, final String tableName) throws SQLException {
+    public static List<ParadoxIndex> listIndexes(final ParadoxConnection conn, final String tableName)
+            throws SQLException {
         final ArrayList<ParadoxIndex> indexes = new ArrayList<>();
         final String indexNamePattern = tableName.substring(0, tableName.lastIndexOf('.')) + ".X??";
         final File[] fileList = conn.getDir().listFiles(new SecondaryIndexFilter(indexNamePattern));
@@ -131,69 +130,48 @@ public final class IndexData {
             buffer.position(0x55);
             index.setReferentialIntegrity(buffer.get());
 
-            if (index.getVersionId() > 4) {
-                // Set the charset.
-                buffer.position(0x6A);
-                index.setCharset(forName("cp" + buffer.getShort()));
+            parseVersionID(buffer, index);
 
-                buffer.position(0x78);
+            parseFields(buffer, index);
+
+            parseSortID(buffer, index);
+            parseIndexName(buffer, index);
+        }
+        return index;
+    }
+
+    /**
+     * Parse fields in index header.
+     *
+     * @param buffer
+     *            the buffer to parse.
+     * @param index
+     *            the paradox index.
+     * @throws SQLException
+     *             in case of parse errors.
+     */
+    private static void parseFields(final ByteBuffer buffer, final ParadoxIndex index) throws SQLException {
+        final ArrayList<ParadoxField> fields = new ArrayList<>();
+        for (int loop = 0; loop < index.getFieldCount(); loop++) {
+            final ParadoxField field = new ParadoxField(loop + 1);
+            field.setType(buffer.get());
+            field.setSize(buffer.get());
+            fields.add(field);
+        }
+
+        if (index.getVersionId() > 4) {
+            if (index.getVersionId() == 0xC) {
+                buffer.position(0x78 + 261 + 4 + 6 * fields.size());
             } else {
-                buffer.position(0x58);
+                buffer.position(0x78 + 83 + 6 * fields.size());
             }
+        } else {
+            buffer.position(0x58 + 83 + 6 * fields.size());
+        }
 
-            final ArrayList<ParadoxField> fields = new ArrayList<>();
-            for (int loop = 0; loop < index.getFieldCount(); loop++) {
-                final ParadoxField field = new ParadoxField(loop + 1);
-                field.setType(buffer.get());
-                field.setSize(buffer.get());
-                fields.add(field);
-            }
+        for (int loop = 0; loop < index.getFieldCount(); loop++) {
+            final ByteBuffer name = allocate(261);
 
-            if (index.getVersionId() > 4) {
-                if (index.getVersionId() == 0xC) {
-                    buffer.position(0x78 + 261 + 4 + 6 * fields.size());
-                } else {
-                    buffer.position(0x78 + 83 + 6 * fields.size());
-                }
-            } else {
-                buffer.position(0x58 + 83 + 6 * fields.size());
-            }
-
-            for (int loop = 0; loop < index.getFieldCount(); loop++) {
-                final ByteBuffer name = allocate(261);
-
-                while (true) {
-                    final byte c = buffer.get();
-                    if (c == 0) {
-                        break;
-                    }
-                    name.put(c);
-                }
-                name.flip();
-                fields.get(loop).setName(index.getCharset().decode(name).toString());
-            }
-            index.setFields(fields);
-
-            final ArrayList<Short> fieldsOrder = new ArrayList<>();
-            for (int loop = 0; loop < index.getFieldCount(); loop++) {
-                fieldsOrder.add(buffer.getShort());
-            }
-            index.setFieldsOrder(fieldsOrder);
-
-            // Sort Order ID
-            final ByteBuffer sortOrderID = allocate(26);
-            while (true) {
-                final byte c = buffer.get();
-                if (c == 0) {
-                    break;
-                }
-                sortOrderID.put(c);
-            }
-            sortOrderID.flip();
-            index.setSortOrderID(index.getCharset().decode(sortOrderID).toString());
-
-            // Index name
-            final ByteBuffer name = allocate(26);
             while (true) {
                 final byte c = buffer.get();
                 if (c == 0) {
@@ -202,11 +180,79 @@ public final class IndexData {
                 name.put(c);
             }
             name.flip();
-            final String tempName = index.getCharset().decode(name).toString();
-            if (tempName.length() != 0) {
-                index.setName(tempName);
-            }
+            fields.get(loop).setName(index.getCharset().decode(name).toString());
         }
-        return index;
+        index.setFields(fields);
+
+        final ArrayList<Short> fieldsOrder = new ArrayList<>();
+        for (int loop = 0; loop < index.getFieldCount(); loop++) {
+            fieldsOrder.add(buffer.getShort());
+        }
+        index.setFieldsOrder(fieldsOrder);
+    }
+
+    /**
+     * Parse and handle the version ID.
+     *
+     * @param buffer
+     *            the buffer to parse.
+     * @param index
+     *            the paradox index.
+     */
+    private static void parseVersionID(final ByteBuffer buffer, final ParadoxIndex index) {
+        if (index.getVersionId() > 4) {
+            // Set the charset.
+            buffer.position(0x6A);
+            index.setCharset(forName("cp" + buffer.getShort()));
+
+            buffer.position(0x78);
+        } else {
+            buffer.position(0x58);
+        }
+    }
+
+    /**
+     * Parse the sort order ID.
+     *
+     * @param buffer
+     *            the buffer to parse.
+     * @param index
+     *            the paradox index.
+     */
+    private static void parseSortID(final ByteBuffer buffer, final ParadoxIndex index) {
+        final ByteBuffer sortOrderID = allocate(26);
+        while (true) {
+            final byte c = buffer.get();
+            if (c == 0) {
+                break;
+            }
+            sortOrderID.put(c);
+        }
+        sortOrderID.flip();
+        index.setSortOrderID(index.getCharset().decode(sortOrderID).toString());
+    }
+
+    /**
+     * Parse the index names.
+     * 
+     * @param buffer
+     *            the buffer to parse.
+     * @param index
+     *            the paradox index.
+     */
+    private static void parseIndexName(final ByteBuffer buffer, final ParadoxIndex index) {
+        final ByteBuffer name = allocate(26);
+        while (true) {
+            final byte c = buffer.get();
+            if (c == 0) {
+                break;
+            }
+            name.put(c);
+        }
+        name.flip();
+        final String tempName = index.getCharset().decode(name).toString();
+        if (tempName.length() != 0) {
+            index.setName(tempName);
+        }
     }
 }
