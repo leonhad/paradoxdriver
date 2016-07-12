e0559e447e6750841041969e7efeb436df8c1624
Leonardo Costa <leonhad@gmail.com>
2016-07-11 21:38:50 -0300

    Added some unit tests.

---
+++ src/test/java/com/googlecode/paradox/results/TypeNameTest.java
@@ -0,0 +1,88 @@
+/*
+ * TypeNameTest.java
+ *
+ * 07/11/2016
+ * Copyright (C) 2016 Leonardo Alves da Costa
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.googlecode.paradox.results;
+
+import java.sql.SQLException;
+import java.sql.Types;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test for {@link TypeName} class.
+ * 
+ * @author Leonardo Alves da Costa
+ * @version 1.0
+ * @since 1.3
+ */
+public class TypeNameTest {
+
+    /**
+     * Gets the class name.
+     * 
+     * @throws SQLException
+     *             in case of errors.
+     */
+    @Test
+    public void testClassName() throws SQLException {
+        Assert.assertEquals("java.io.InputStream", TypeName.getClassName(Types.BINARY));
+    }
+
+    /**
+     * Test for unsupported type.
+     * 
+     * @throws SQLException
+     *             if there is no errors.
+     */
+    @Test(expected = SQLException.class)
+    public void testClassNameUnsupported() throws SQLException {
+        TypeName.getClassName(99);
+    }
+
+    /**
+     * Gets the field name.
+     * 
+     * @throws SQLException
+     *             in case of errors.
+     */
+    @Test
+    public void testFieldName() throws SQLException {
+        Assert.assertEquals("BINARY", TypeName.getName(Types.BINARY));
+    }
+
+    /**
+     * Test for unsupported type name.
+     * 
+     * @throws SQLException
+     *             if there is no errors.
+     */
+    @Test(expected = SQLException.class)
+    public void testNameUnsupported() throws SQLException {
+        TypeName.getName(99);
+    }
+
+    /**
+     * Test for SQL Type.
+     */
+    @Test
+    public void testSQLType() {
+        Assert.assertEquals(Types.BINARY, TypeName.BINARY.getSQLType());
+    }
+}
