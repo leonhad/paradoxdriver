576ffca8d71deeafc9f5ce2ea020056c21cce2d3
Leonardo Costa <leonhad@gmail.com>
2020-06-16 22:25:08 -0300

    Fixed copyright message.

--- src/main/java/com/googlecode/paradox/ParadoxConnection.java
+++ src/main/java/com/googlecode/paradox/ParadoxConnection.java
@@ -1,10 +1,12 @@
 /*
- * ParadoxConnection.java 03/14/2009 Copyright (C) 2009 Leonardo Alves da Costa This program is free software: you can
- * redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in
- * the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a
- * copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * Copyright (C) 2009 Leonardo Alves da Costa
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any
+ * later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+ * License for more details. You should have received a copy of the GNU General Public License along with this
+ * program. If not, see <http://www.gnu.org/licenses/>.
  */
 package com.googlecode.paradox;
 
@@ -15,22 +17,7 @@ import com.googlecode.paradox.utils.filefilters.DirectoryFilter;
 
 import java.io.File;
 import java.nio.charset.Charset;
-import java.sql.Array;
-import java.sql.Blob;
-import java.sql.CallableStatement;
-import java.sql.Clob;
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.NClob;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.SQLFeatureNotSupportedException;
-import java.sql.SQLWarning;
-import java.sql.SQLXML;
-import java.sql.Savepoint;
-import java.sql.Statement;
-import java.sql.Struct;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.Properties;
@@ -222,7 +209,7 @@ public final class ParadoxConnection implements Connection {
      */
     @Override
     public Statement createStatement(final int resultSetType, final int resultSetConcurrency,
-            final int resultSetHoldability) {
+                                     final int resultSetHoldability) {
         return this.createStatement();
     }
 
@@ -505,7 +492,7 @@ public final class ParadoxConnection implements Connection {
      */
     @Override
     public CallableStatement prepareCall(final String sql, final int resultSetType, final int resultSetConcurrency,
-            final int resultSetHoldability) throws SQLException {
+                                         final int resultSetHoldability) throws SQLException {
         return this.prepareCall(sql);
     }
 
@@ -530,7 +517,7 @@ public final class ParadoxConnection implements Connection {
      */
     @Override
     public CallableStatement prepareCall(final String sql, final int resultSetType,
-            final int resultSetConcurrency) throws SQLException {
+                                         final int resultSetConcurrency) throws SQLException {
         return this.prepareCall(sql);
     }
 
@@ -539,7 +526,7 @@ public final class ParadoxConnection implements Connection {
      */
     @Override
     public PreparedStatement prepareStatement(final String sql, final int resultSetType, final int resultSetConcurrency,
-            final int resultSetHoldability) throws SQLException {
+                                              final int resultSetHoldability) throws SQLException {
         return this.prepareStatement(sql);
     }
 
@@ -629,7 +616,7 @@ public final class ParadoxConnection implements Connection {
      */
     @Override
     public PreparedStatement prepareStatement(final String sql, final int resultSetType,
-            final int resultSetConcurrency) throws SQLException {
+                                              final int resultSetConcurrency) throws SQLException {
         return this.prepareStatement(sql);
     }
 
