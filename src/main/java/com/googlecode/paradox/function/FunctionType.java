30a024f301b30efd8571186b4cf9af8fb61fc190
Leonardo Costa <leonhad@gmail.com>
2020-07-20 23:24:26 -0300

    Fixed some sonar issues.

--- src/main/java/com/googlecode/paradox/function/FunctionType.java
+++ src/main/java/com/googlecode/paradox/function/FunctionType.java
@@ -10,12 +10,30 @@
  */
 package com.googlecode.paradox.function;
 
+/**
+ * Describe the function type to use in metadata.
+ *
+ * @version 1.0
+ * @since 1.6.0
+ */
 public enum FunctionType {
+    /**
+     * A string function.
+     */
     STRING,
 
+    /**
+     * A numeric function.
+     */
     NUMERIC,
 
+    /**
+     * A system function.
+     */
     SYSTEM,
 
+    /**
+     * A time/date function.
+     */
     TIME_DATE
 }
