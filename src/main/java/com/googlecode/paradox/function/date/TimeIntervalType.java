e13180ed0eeb50cef1245de1a2fc4404f3d6c9ed
Leonardo Costa <leonhad@gmail.com>
2020-07-28 15:06:21 -0300

    Code cleanups.

--- src/main/java/com/googlecode/paradox/function/date/TimeIntervalType.java
+++ src/main/java/com/googlecode/paradox/function/date/TimeIntervalType.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright (C) 2009 Leonardo Alves da Costa
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any
+ * later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+ * License for more details. You should have received a copy of the GNU General Public License along with this
+ * program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.googlecode.paradox.function.date;
 
 import com.googlecode.paradox.utils.Utils;
@@ -9,15 +19,55 @@ import com.googlecode.paradox.utils.Utils;
  * @since 1.6.0
  */
 public enum TimeIntervalType {
+
+    /**
+     * Milliseconds.
+     */
     MILLISECOND,
+
+    /**
+     * Seconds.
+     */
     SECOND,
+
+    /**
+     * Minutes.
+     */
     MINUTE,
+
+    /**
+     * Hours.
+     */
     HOUR,
+
+    /**
+     * Day in 24h.
+     */
     DAY,
+
+    /**
+     * Day of the year.
+     */
     DAYOFYEAR,
+
+    /**
+     * Week number.
+     */
     WEEK,
+
+    /**
+     * Month (1-12).
+     */
     MONTH,
+
+    /**
+     * Quarter.
+     */
     QUARTER,
+
+    /**
+     * Year.
+     */
     YEAR;
 
     /**
