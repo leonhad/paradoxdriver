dd2e3f639ad6374dea7e0bd1471466ae176eec1a
Leonardo Costa <leonhad@gmail.com>
2020-08-10 00:04:03 -0300

    Improved VIEW support.

--- src/main/resources/com/googlecode/paradox/information_schema/columns.sql
+++ src/main/resources/com/googlecode/paradox/information_schema/columns.sql
@@ -1,30 +1,33 @@
-select c."catalog" as table_catalog,
-       c."schema"  as table_schema,
-       "table"     as table_name,
-       c.name      as column_name,
-       ordinal     as ordinal_position,
-    varchar (null) as column_default,
-    is_nullable,
-    c.type as data_type,
-    maximum_length as character_maximum_length,
-    "octet_length" as character_octet_length,
-    "precision" as numeric_precision,
-    10 as numeric_precision_radix,
-    scale as numeric_scale,
-    numeric (null) as datetime_precision,
-    numeric (null) as interval_type,
-    numeric (null) as interval_precision,
-    varchar (null) as character_set_catalog,
-    varchar (null) as character_set_schema,
-    t.charset as character_set_name,
-    varchar (null) as collation_catalog,
-    varchar (null) as collation_schema,
-    varchar (null) as collation_name,
-    varchar (null) as domain_catalog,
-    varchar (null) as domain_schema,
-    varchar (null) as domain_name
+select  c."catalog" as table_catalog,
+        c."schema"  as table_schema,
+        "table"     as table_name,
+        c.name      as column_name,
+        ordinal     as ordinal_position,
+        varchar (null) as column_default,
+        is_nullable,
+        c.type as data_type,
+        maximum_length as character_maximum_length,
+        "octet_length" as character_octet_length,
+        "precision" as numeric_precision,
+        10 as numeric_precision_radix,
+        scale as numeric_scale,
+        numeric (null) as datetime_precision,
+        numeric (null) as interval_type,
+        numeric (null) as interval_precision,
+        s.default_character_set_catalog as character_set_catalog,
+        s.default_character_set_schema as character_set_schema,
+        t.charset as character_set_name,
+        varchar (null) as collation_catalog,
+        varchar (null) as collation_schema,
+        varchar (null) as collation_name,
+        varchar (null) as domain_catalog,
+        varchar (null) as domain_schema,
+        varchar (null) as domain_name
 from information_schema.pdx_columns c
-    inner join information_schema.pdx_tables t
-on c.catalog = t.catalog
-    and c.schema = t.schema
-    and c.table = t.name
+         inner join information_schema.pdx_tables t
+                    on c.catalog = t.catalog
+                        and c.schema = t.schema
+                        and c.table = t.name
+         inner join information_schema.pdx_schemata s
+                    on c.catalog = s.catalog_name
+                        and c.schema = s.schema_name
